b95ddd93766f828db7f6538fd49f1b8f0e354c1b
amir183258 <amirhossein183258@gmail.com>
2026-07-06 19:18:41 +0330

    lib: add sock_cmp_port (page 122)

---
+++ lib/sock_cmp_port.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *
+ * Project:  Stevens Book - Big Library
+ * Purpose:  This function compares 2 sockaddr ports and return the result.
+ * Author:   A. H. Ebrahimi <amirhossein183258 at gmail.com>
+ *
+ ****************************************************************************/
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "unp.h"
+
+int sock_cmp_port(const struct sockaddr *sa1, const struct sockaddr *sa2, socklen_t salen) {
+	if (sa1->sa_family != sa2->sa_family)
+		return -1;
+
+	switch (sa1->sa_family) {
+	case AF_INET:
+		return ( ((struct sockaddr_in *) sa1)->sin_port == // <netinet/in.h>
+				((struct sockaddr_in *) sa2)->sin_port);
+#ifdef IPV6
+	case AF_INET6:
+		return ( ((struct sockaddr_in6 *) sa1)->sin6_port == // <netinet/in.h>
+				((struct sockaddr_in6 *) sa2)->sin6_port);
+#endif
+	}
+
+	return -1;
+}
--- lib/unp.h
+++ lib/unp.h
@@ -42,6 +42,9 @@ int Sock_bind_wild(int sockfd, int family);
 /* sock_cmp_addr.c */
 int sock_cmp_addr(const SA *sa1, const SA *sa2, socklen_t salen);
 
+/* sock_cmp_port.c */
+int sock_cmp_port(const SA *sa1, const SA *sa2, socklen_t salen);
+
 /* wrapsock.c */
 int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr);
 int Socket(int family, int type, int protocol);
