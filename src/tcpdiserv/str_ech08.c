b98c61e740decac0e22dd4995d6c327c5b06e285
amir183258 <amirhossein183258@gmail.com>
2026-07-10 20:50:07 +0330

    app: add str_ech08.c to create a sum server (page 199)

---
+++ src/tcpdiserv/str_ech08.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *
+ * Project:  Stevens Book - Echo Server Project
+ * Purpose:  This function adds two numbers. This is for the server.
+ * Author:   A. H. Ebrahimi <amirhossein183258 at gmail.com>
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+
+#include "unp.h"
+
+void str_echo(int sockfd) {
+	long arg1, arg2;
+	ssize_t n;
+	char line[MAXLINE];
+
+	for ( ; ; ) {
+		if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
+			return; // connection closed by other end
+
+		if (sscanf(line, "%ld%ld", &arg1, &arg2) == 2)
+			snprintf(line, sizeof(line), "%ld\n", arg1 + arg2);
+		else
+			snprintf(line, sizeof(line), "input error\n");
+
+		n = strlen(line);
+		Writen(sockfd, line, n);
+	}
+
+	return;
+}
