ed56bbe92b0db14064e11af674dc194373353c6b
amir183258 <amirhossein183258@gmail.com>
2026-07-26 18:36:08 +0330

    app: add dgcliaddr.c and udpcli02.c (page 308)

---
+++ src/udpcliserv/dgcliaddr.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *
+ * Project:  Stevens Book - Echo Server Project in UDP
+ * Purpose:  This functions improves normal dg_cli function. It verifies
+ * 	     returned socket address for UDP echo server.
+ * Author:   A. H. Ebrahimi <amirhossein183258 at gmail.com>
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "unp.h"
+
+void dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen) {
+	int n;
+	char sendline[MAXLINE], recvline[MAXLINE + 1];
+	socklen_t len;
+	struct sockaddr *preply_addr;
+
+	preply_addr = Malloc(servlen);
+
+	while (Fgets(sendline, MAXLINE, fp) != NULL) {
+		Sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+
+		len = servlen;
+		n = Recvfrom(sockfd, recvline, MAXLINE, 0, preply_addr, &len);
+		if (len != servlen || memcmp(pservaddr, preply_addr, len) != 0) {
+			printf("reply from %s (ignored)\n", Sock_ntop(preply_addr, len));
+			continue;
+		}
+
+		recvline[n] = 0; // null terminate
+		Fputs(recvline, stdout);
+	}
+}
