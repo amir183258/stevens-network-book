c6a8451a493eb8126499a88a8e8c5e9d9c0ca692
amir183258 <amirhossein183258@gmail.com>
2026-08-05 23:57:05 +0330

    lib: add initial file of host_serv() (page 406)

---
+++ lib/host_serv.c
@@ -0,0 +1,11 @@
+/******************************************************************************
+ *
+ * Project:  Stevens Book - Big Library
+ * Purpose:  
+ * Author:   A. H. Ebrahimi <amirhossein183258 at gmail.com>
+ *
+ ****************************************************************************/
+
+#include <netdb.h>
+
+#include "unp.h"
