e6d25ceb44f30ea80e8118c213bc22c0323488a3
amir183258 <amirhossein183258@gmail.com>
2026-08-08 20:31:19 +0330

    lib: add udp_connect to the lib (page 422)

---
+++ lib/udp_connect.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *
+ * Project:  Stevens Book - Big Library
+ * Purpose:  This function creates a connected UDP socket.
+ * Author:   A. H. Ebrahimi <amirhossein183258 at gmail.com>
+ *
+ ****************************************************************************/
+
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include "unp.h"
+
+int udp_connect(const char *host, const char *serv) {
+	int sockfd, n;
+	struct addrinfo hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		err_quit("udp_connect error for %s, %s: %s", host, serv, gai_strerror(n));
+	ressave = res;
+
+	do {
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sockfd < 0)
+			continue; // ignore this one
+
+		if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0)
+			break; // success
+
+		Close(sockfd); // ignore this one
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL)
+		err_sys("udp_connect error for %s, %s", host, serv);
+
+	freeaddrinfo(ressave);
+
+	return sockfd;
+}
