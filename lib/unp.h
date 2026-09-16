1969c24e80c66713187ea259ace4ab38419ef7e4
amir183258 <amirhossein183258@gmail.com>
2026-09-16 20:19:17 +0330

    lib: add write_fd to write descriptors using recvmsg() (page 526)

--- lib/unp.h
+++ lib/unp.h
@@ -195,4 +195,8 @@ int Readable_timeo(int fd, int sec);
 
 /* read_fd.c */
 ssize_t Read_fd(int fd, void *ptr, size_t nbytes, int *recvd);
+
+/* write_fd.c */
+ssize_t Write_fd(int fd, void *ptr, size_t nbytes, int sendfd);
+
 #endif
