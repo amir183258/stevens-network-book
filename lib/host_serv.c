f90fec9404aa496536feaa5e38d6b0723cf0c968
amir183258 <amirhossein183258@gmail.com>
2026-09-23 23:38:59 +0330

    lib: add Host_serv() wrapper function to the lib (page 556)

--- lib/host_serv.c
+++ lib/host_serv.c
@@ -27,3 +27,25 @@ struct addrinfo* host_serv(const char *host, const char *serv, int family, int s
 
 	return res; // return pointer to first on linked list
 }
+
+/*
+ * There is no easy way to pass back the integer return code from
+ * getaddrinfo() in the function above, short of adding another argument
+ * that is a pointer, so the easiest way to provide the wrapper function
+ * is just duplicate the simple function as we do here.
+ */
+
+struct addrinfo* Host_serv(const char *host, const char *serv, int family, int socktype) {
+	int n;
+	struct addrinfo hints, *res;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_flags = AI_CANONNAME; // always return canonical name
+	hints.ai_family = family; // AF_UNSPECT, AF_INET, AF_INET6, etc.
+	hints.ai_socktype= socktype; // 0, SOCK_STREAM, SOCK_DGRAM, etc.
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		return NULL;
+
+	return res; // return pointer to first on linked list
+}
--- lib/unp.h
+++ lib/unp.h
@@ -164,6 +164,7 @@ void err_quit(const char *fmt, ...);
 
 /* host_serv.c */
 struct addrinfo* host_serv(const char *host, const char *serv, int family, int socktype);
+struct addrinfo* Host_serv(const char *host, const char *serv, int family, int socktype);
 
 /* tcp_connect.c */
 int tcp_connect(const char *host, const char *serv);
